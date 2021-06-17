4788f5156f3f16de182d5b057cacd98c9abad928
3kanAlpha <39447635+3kanAlpha@users.noreply.github.com>
2021-06-17 16:18:51 +0900

    とりあえず仮公開

--- assets/main.js
+++ assets/main.js
@@ -1,12 +1,38 @@
 
+const app = Vue.createApp({
+    data() {
+      return {
+        items: []
+      }
+    },
+    mounted() {
+        console.log("App mounted.");
+    }
+});
+const vm = app.mount('#list-parent');
+
 let data;
 let videos;
 
 $(function() {
-    $.getJSON('assets/data_20210617_123937.json', function(e) {
-        data = e;
-        videos = data['videos']
-
-        console.log("JSON loaded successfully. size=" + videos.length)
-    });
+    $.when(
+        $.getJSON('assets/data_20210617_123937.json', function(e) {
+            data = e;
+            videos = data['videos'];
+    
+            console.log("JSON loaded successfully. size=" + videos.length);
+        })
+    ).done(buildList);
 });
+
+function buildList() {
+    for (let i = 0; i < videos.length; i++) {
+        const v = videos[i];
+        const a = v.snippet.title;
+        const b = v.snippet.channelTitle;
+        const c = v.snippet.thumbnails.medium.url;
+        const l = "https://www.youtube.com/watch?v=" + v.id.videoId;
+
+        vm.items.push({ title: a, name: b, img: c, link: l });
+    }
+}
