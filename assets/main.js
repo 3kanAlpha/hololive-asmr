45acc58383c290af975564e353ca7f03ca768ef4
3kanAlpha <39447635+3kanAlpha@users.noreply.github.com>
2021-08-03 14:25:00 +0900

    データベース更新 0803

--- assets/main.js
+++ assets/main.js
@@ -13,7 +13,7 @@ const app = Vue.createApp({
 const vm = app.mount('#list-parent');
 
 // データベースを更新した日時
-const updatedDate = "20210727-202137";  // アプデしたらここだけ書き換えればよい
+const updatedDate = "20210803-142346";  // アプデしたらここだけ書き換えればよい
 const jsonPath = 'assets/data-' + updatedDate + '.json';
 
 function showUpdatedDate() {
